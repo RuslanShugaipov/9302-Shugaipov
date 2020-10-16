e8c76777246901293e3157db93cbcba7f6c41ddf
RuslanShugaipov <58910717+RuslanShugaipov@users.noreply.github.com>
2020-10-16 22:00:01 +0300

    Add files via upload

---
+++ АлгиСтр(Лаба1).cpp
@@ -0,0 +1,24 @@
+﻿#include "LinkedList.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	LinkedList a,b;
+	for (int i = -1; i <= 20; i++)
+		a.push_back(i);
+	for (int i = -3; i <= 20; i++)
+		b.push_back(i);
+	a.print_to_console();
+	cout << endl;
+	b.print_to_console();
+	cout << endl;
+	cout << "The number of the first occurrence of one list in another: " << a.find_first(b, a) << "\n\n";
+	a.set(5, 78);
+	cout << endl;
+	a.print_to_console();
+	a.clear();
+	b.clear();
+	return 0;
+}
